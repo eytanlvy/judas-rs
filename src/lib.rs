cec7709af5a739eac23fe24c4b304feae4c7b0a4
Eytan Levy <eytanlevy@proton.me>
2024-03-01 15:45:46 +0100

    archi clean

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod maths;
+mod fhe;
+mod utils;
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_hello_world() {
+        let s = String::from("Hello, world!");
+        println!("{}", s);
+    }
+}
